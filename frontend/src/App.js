57f1d5fc4ed3ac9a70caa08f2230e036c5cc5410
MMPjayarathne <pramudithajayarathna@gmail.com>
2023-04-12 21:44:11 +0530

    feat: create Register frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router,Route,Routes } from "react-router-dom";
 import SignIn from "./pages/SignIn.jsx"
+import Register from "./pages/Register.jsx"
 
 function App() {
   return (
     <Router>
         <Routes>
         <Route path="/" element={<SignIn/>}/>
+        <Route path="/signup" element={<Register/>}/>
       </Routes>
       </Router>
   );
